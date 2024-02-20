e3c54c8a4dd8144ec895eb41bb1d9d27a798554f
DhruvZopsmart <102145884+DhruvZopsmart@users.noreply.github.com>
2024-02-20 22:06:53 +0530

    remove condition from chat provider

--- src/Context/chatContext.js
+++ src/Context/chatContext.js
@@ -12,7 +12,7 @@ const ChatProvider = ({ children }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo") || "");
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
     setUser(userInfo);
 
     if (!userInfo) navigate("/");
